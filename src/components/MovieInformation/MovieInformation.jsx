288e5b5a027d0c8550b5b3b0849eb67360b63d33
Giorgi <grgpaikidze@gmail.com>
2024-05-12 10:49:54 +0400

    feat: add movie information

--- src/components/MovieInformation/MovieInformation.jsx
+++ src/components/MovieInformation/MovieInformation.jsx
@@ -1,7 +1,91 @@
 import React from "react";
+import { useParams, Link } from "react-router-dom";
+import { Box, CircularProgress, Grid, Rating, Typography } from "@mui/material";
+import { useDispatch } from "react-redux";
+
+import { useGetMovieQuery } from "../../services/TMDB";
+import useStyles from "./styles";
+import genreIcons from "../../assets/genres";
+import { selectGenreOrCategory } from "../../features/currentGenreOrCategory";
 
 const MovieInformation = () => {
-  return <div>MovieInformation</div>;
+  const { id } = useParams();
+  const { data, error, isFetching } = useGetMovieQuery(id);
+  const dispatch = useDispatch();
+  const classes = useStyles();
+
+  if (isFetching) {
+    return (
+      <Box display="flex" justifyContent="center" alignItems="center">
+        <CircularProgress size="8rem" />
+      </Box>
+    );
+  }
+
+  if (error) {
+    return (
+      <Box display="flex" justifyContent="center" alignItems="center">
+        <Link to="/">Something has gone wrong - Go back</Link>
+      </Box>
+    );
+  }
+
+  return (
+    <Grid container className={classes.containerSpaceAround}>
+      <Grid item sm={12} lg={4}>
+        <img
+          className={classes.poster}
+          src={`https://image.tmdb.org/t/p/w500/${data?.poster_path}`}
+          alt={data?.title}
+        />
+      </Grid>
+      <Grid item container direction="column" lg={7}>
+        <Typography variant="h3" align="center" gutterBottom>
+          {data?.title} ({data?.release_date.split("-")[0]})
+        </Typography>
+        <Typography variant="h5" align="center" gutterBottom>
+          {data?.tagline}
+        </Typography>
+        <Grid item className={classes.containerSpaceAround}>
+          <Box display="flex" align="center">
+            <Rating readOnly value={data.vote_average / 2} />
+            <Typography
+              variant="subtitle1"
+              gutterBottom
+              style={{ marginLeft: "10px" }}
+            >
+              {data?.vote_average} / 10
+            </Typography>
+          </Box>
+          <Typography variant="h6" align="center" gutterBottom>
+            {data?.runtime}min
+            {data?.spoken_languages.length > 0
+              ? ` / ${data?.spoken_languages[0].name}`
+              : ""}
+          </Typography>
+        </Grid>
+        <Grid item className={classes.genresContainer}>
+          {data?.genres?.map((genre) => (
+            <Link
+              className={classes.links}
+              key={genre?.name}
+              to="/"
+              onClick={() => dispatch(selectGenreOrCategory(genre.id))}
+            >
+              <img
+                src={genreIcons[genre?.name.toLowerCase()]}
+                className={classes.genreImage}
+                height={30}
+              />
+              <Typography variant="subtitle1" color="textPrimary">
+                {genre?.name}
+              </Typography>
+            </Link>
+          ))}
+        </Grid>
+      </Grid>
+    </Grid>
+  );
 };
 
 export default MovieInformation;
